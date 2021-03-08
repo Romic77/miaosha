e37c41a9c416fbccc85acc9eeb956e6b1de06cbc
rom1c <280385556@qq.com>
2021-03-08 23:43:02 +0800

    添加rocketmq依赖

--- miaosha-order/src/main/java/com/miaosha/miaoshaproduct/MiaoshaOrderApplication.java
+++ miaosha-order/src/main/java/com/miaosha/miaoshaproduct/MiaoshaOrderApplication.java
@@ -1,15 +1,18 @@
 package com.miaosha.miaoshaproduct;
 
+import com.miaosha.miaoshaproduct.rocketmq.MySource;
 import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableAutoDataSourceProxy
+//@EnableAutoDataSourceProxy
+@EnableBinding({MySource.class})
 public class MiaoshaOrderApplication {
 
     public static void main(String[] args) {
--- miaosha-product/src/main/java/com/miaosha/miaoshaproduct/MiaoshaProductApplication.java
+++ miaosha-product/src/main/java/com/miaosha/miaoshaproduct/MiaoshaProductApplication.java
@@ -1,14 +1,17 @@
 package com.miaosha.miaoshaproduct;
 
+import com.miaosha.miaoshaproduct.rocketmq.MySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableBinding({MySource.class})
 public class MiaoshaProductApplication {
 
     public static void main(String[] args) {
