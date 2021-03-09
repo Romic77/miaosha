a3c7fb9d94b8c3ee3bb55c6b8ff2dc5c38fbe50c
chenqi <280385556>
2021-03-09 13:00:04 +0800

    修改rocketmq 使用方式,不通过stream接入, 使用 springboot starter rocketqmq

--- miaosha-product/src/main/java/com/miaosha/miaoshaproduct/rocketmq/ReceiveService.java
+++ miaosha-product/src/main/java/com/miaosha/miaoshaproduct/rocketmq/ReceiveService.java
@@ -1,39 +1,27 @@
 package com.miaosha.miaoshaproduct.rocketmq;
 
 import com.miaosha.miaoshaproduct.domain.dao.ProductMapper;
-import com.miaosha.miaoshaproduct.domain.dto.ProductDTO;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
-public class ReceiveService {
-    @Autowired
-    private ProductMapper productMapper;
+@RocketMQMessageListener(consumerGroup = "consumer_test_topic", topic = "test-topic1")
+public class ReceiveService implements RocketMQListener<String> {
 
     /**
      * logger
      */
     private static final Logger logger = LoggerFactory.getLogger(ReceiveService.class);
+    @Autowired
+    private ProductMapper productMapper;
 
-    @StreamListener("input1")
-    public void receiveInput1(String receiveMsg) {
-        logger.info("input1 接收到了消息：" + receiveMsg);
-    }
 
-    @StreamListener("input2")
-    public void receiveInput2(String receiveMsg) {
-        logger.info("input2 接收到了消息：" + receiveMsg);
-    }
-
-    @StreamListener("input3")
-    public void receiveInput3(@Payload ProductDTO productDTO) {
-        logger.info("input3 接收到了消息：" + productDTO);
-        productMapper.updateByPrimaryKeySelective(productDTO);
+    @Override
+    public void onMessage(String s) {
+        logger.info("开始消费消息:{}", s);
     }
 }
